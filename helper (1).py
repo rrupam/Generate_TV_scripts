d0fc8272c3dfdcc71611024917a5b64acdab0e08
Rishika Rupam <rishika.r@gmail.com>
2019-05-15 10:55:58 +0200

    Add files via upload

---
+++ helper (1).py
@@ -0,0 +1,136 @@
+import os
+import pickle
+import torch
+import torch.nn as nn
+
+
+SPECIAL_WORDS = {'PADDING': '<PAD>'}
+
+
+def load_data(path):
+    """
+    Load Dataset from File
+    """
+    input_file = os.path.join(path)
+    with open(input_file, "r") as f:
+        data = f.read()
+
+    return data
+
+
+def preprocess_and_save_data(dataset_path, token_lookup, create_lookup_tables):
+    """
+    Preprocess Text Data
+    """
+    text = load_data(dataset_path)
+    
+    # Ignore notice, since we don't use it for analysing the data
+    text = text[81:]
+
+    token_dict = token_lookup()
+    for key, token in token_dict.items():
+        text = text.replace(key, ' {} '.format(token))
+
+    text = text.lower()
+    text = text.split()
+
+    vocab_to_int, int_to_vocab = create_lookup_tables(text + list(SPECIAL_WORDS.values()))
+    int_text = [vocab_to_int[word] for word in text]
+    pickle.dump((int_text, vocab_to_int, int_to_vocab, token_dict), open('preprocess.p', 'wb'))
+
+
+def load_preprocess():
+    """
+    Load the Preprocessed Training data and return them in batches of <batch_size> or less
+    """
+    return pickle.load(open('preprocess.p', mode='rb'))
+
+
+def save_model(filename, decoder):
+    save_filename = os.path.splitext(os.path.basename(filename))[0] + '.pt'
+    torch.save(decoder, save_filename)
+
+
+def load_model(filename):
+    save_filename = os.path.splitext(os.path.basename(filename))[0] + '.pt'
+    class RNN(nn.Module):
+    
+    def __init__(self, vocab_size, output_size, embedding_dim, hidden_dim, n_layers, dropout=0.5):
+        """
+        Initialize the PyTorch RNN Module
+        :param vocab_size: The number of input dimensions of the neural network (the size of the vocabulary)
+        :param output_size: The number of output dimensions of the neural network
+        :param embedding_dim: The size of embeddings, should you choose to use them        
+        :param hidden_dim: The size of the hidden layer outputs
+        :param dropout: dropout to add in between LSTM/GRU layers
+        """
+        super(RNN, self).__init__()
+        
+        # set class variables
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.hidden_dim = hidden_dim
+        
+        # define model layers
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, 
+                            dropout=dropout, batch_first=True)
+        self.dropout = nn.Dropout(0.3)
+        self.fc = nn.Linear(hidden_dim, output_size)
+        
+    
+    def forward(self, nn_input, hidden):
+        """
+        Forward propagation of the neural network
+        :param nn_input: The input to the neural network
+        :param hidden: The hidden state        
+        :return: Two Tensors, the output of the neural network and the latest hidden state
+        """
+        # TODO: Implement function   
+        batch_size = nn_input.size(0)
+
+        # embeddings and lstm_out
+        nn_input = nn_input.long()
+        embeds = self.embedding(nn_input)
+        lstm_output, hidden = self.lstm(embeds, hidden)
+        
+        # stack up lstm outputs
+        lstm_output = lstm_output.contiguous().view(-1, self.hidden_dim)
+        
+        # dropout and fully-connected layer
+        output = self.dropout(lstm_output)
+        output = self.fc(output)
+        
+        # reshape into (batch_size, seq_length, output_size)
+        output = output.view(batch_size, -1, self.output_size)
+        # get last batch
+        output = output[:, -1]
+
+        # return one batch of output word scores and the hidden state
+        return output, hidden
+    
+    
+    def init_hidden(self, batch_size):
+        '''
+        Initialize the hidden state of an LSTM/GRU
+        :param batch_size: The batch_size of the hidden state
+        :return: hidden state of dims (n_layers, batch_size, hidden_dim)
+        '''
+        weight = next(self.parameters()).data
+        
+        if (train_on_gpu):
+            hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda(),
+                  weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda())
+        else:
+            hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_(),
+                      weight.new(self.n_layers, batch_size, self.hidden_dim).zero_())
+        
+        return hidden
+    model = RNN()
+    if torch.cuda.is_available():
+        map_location=lambda storage, loc: storage.cuda()
+    else:
+        map_location='cpu'
+        print('cpu time')
+    return model.load_state_dict(torch.load(save_filename, map_location=map_location))
+
